af15f034a3ee96d548faf1123b9a67efdf90fcbf
zwelinux <zwelinux@gmail.com>
2023-05-01 15:02:54 +0630

    1_5_2023_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NewComponent from './newComponent';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Footer = () => {
+  return(
+    <div>
+      This is footer
     </div>
-  );
+  )
+}
+
+const userInfo = {
+  userName : 'zwelinux',
+  status: 'online',
+  email : 'zwelinux@gmail.com',
+}
+
+const svtTracks = [
+  {title: 'super', id: '1'},
+  {title: 'fml', id: '2'},
+  {title: 'april shower', id: '3'},
+]
+// hardcode 
+
+const App = () => {
+
+  const doSomething = () => {
+    alert("Hello from the other side");
+  }
+
+  return(
+
+      // JSX <> </> in React 
+      <div className='main'>
+        <NewComponent />
+        <div className='mainPage'>
+          Main Page
+          <hr />
+          <h2>userName : {userInfo.userName}</h2>
+          <h2>userStatus : {userInfo.status}</h2>
+          <h2>userEmail : {userInfo.email}</h2>
+          <hr />
+          <h1>SVT Tracks</h1>
+
+          {svtTracks.map( x => 
+            <li key={x.id}>{x.title}</li>  
+          )} 
+
+          <button onClick={doSomething}>show something</button>
+
+        </div>
+        <Footer />
+      </div>
+  )
 }
 
 export default App;
---
+++ src/newComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewComponent = () => {
+    return(
+        <div>
+            <h2>New component</h2>
+        </div>
+    )
+}
+
+export default NewComponent
